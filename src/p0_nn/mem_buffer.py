a05cd570d1cbb3737b694701131b587dfb7277de
Matteo Franceschini <36609943+matteof04@users.noreply.github.com>
2026-04-24 14:05:07 +0200

    Revert "Formattazione e base di training"
    This reverts commit ab7c463381595d180301ce987fd74700efb6ed33.

--- src/p0_nn/mem_buffer.py
+++ src/p0_nn/mem_buffer.py
@@ -1,15 +1,13 @@
-from math import ceil
-
 import torch
-from torch.utils.data import DataLoader, Dataset
-
+from torch.utils.data import Dataset, DataLoader 
+from math import ceil 
 
 class MemBuffer(Dataset):
     def __init__(self, size=0, max_size=1e3, dropout_size=None):
         self.size = size
         self.max_size = max_size
         if dropout_size == None:
-            self.dropout = ceil(max_size / 10)
+            self.dropout = ceil(max_size/10)
         else:
             self.dropout = dropout_size
 
@@ -19,13 +17,13 @@ class MemBuffer(Dataset):
     def __len__(self):
         return self.size
 
-    def __getitem__(self, index):
-        return self.x[index], self.y[index]
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
 
     def add(self, x, y):
         if self.size == self.max_size:
-            self.x = self.x[self.dropout :]
-            self.y = self.y[self.dropout :]
+            self.x = self.x[self.dropout:]
+            self.y = self.y[self.dropout:]
         else:
             self.size += 1
 
@@ -33,24 +31,18 @@ class MemBuffer(Dataset):
         self.y.append(y)
         return
 
-
 if __name__ == "__main__":
     bs = 2
     buffer = MemBuffer(max_size=5)
-
-    print("\n-------------- filling buffer")
+    
+    print('\n-------------- filling buffer')
     for i in range(7):
-        buffer.add(i, i + 100)
+        buffer.add(i, i+100)
         print(buffer.x, buffer.y)
+    
+    dl = DataLoader(dataset=buffer, batch_size=bs, shuffle=True, generator=torch.Generator(device='cpu'))
 
-    dl = DataLoader(
-        dataset=buffer,
-        batch_size=bs,
-        shuffle=True,
-        generator=torch.Generator(device="cpu"),
-    )
-
-    print("\n-------------- dataloading")
+    print('\n-------------- dataloading')
     for i in range(3):
         data = next(iter(dl))
-        print("data: ", data)
+        print('data: ', data)
