0341a2b77cc8ee37c4ffb648a721239ab29c3495
junior07 <vasco.chironda@studio.unibo.it>
2026-05-01 17:31:57 +0200

    Refactor NN class to include dynamic hidden layers and optimizer setup

--- src/p0_nn/q1.py
+++ src/p0_nn/q1.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 from time import sleep
-from torch import nn
 import torch
+import torch.nn as nn
 from numpy import sin, cos
 from math import ceil
 import matplotlib.pyplot as plt
@@ -24,24 +24,36 @@ def get_cartesian(env):
 
 # add parameters for number of layers, layer size
 class NN(nn.Module):
-    def __init__(self, env):
-        nn.Module.__init__(self)
-        # add env
-        self.env = env
+    def __init__(self, env, n_layers=2, layer_size=32, lr=1e-3, n_iter=100, max_epochs=100):
+        super().__init__()
 
-        # add n_layers, layer_size, lr, n_iter, max_epochs
+        # environment and hyperparameters
+        self.env = env
         self.n_layers = n_layers
         self.layer_size = layer_size
+        self.lr = lr
+        self.n_iter = n_iter
+        self.max_epochs = max_epochs
 
-        # NN 
-        self.nn = nn.Sequential(
-                nn.Linear(4, 5),
-                nn.ReLU(),
-                nn.Linear(5, 4),
-                nn.ReLU(),
-                )
+        # input / output sizes from environment spaces
+        input_dim = int(torch.tensor(env.observation_space.shape).prod().item())
+        if isinstance(env.action_space, gym.spaces.Discrete):
+            output_dim = env.action_space.n
+        else:
+            output_dim = int(torch.tensor(env.action_space.shape).prod().item())
 
-        return
+        # build dynamic hidden layers
+        layers = [nn.Linear(input_dim, self.layer_size), nn.ReLU()]
+        for _ in range(max(0, self.n_layers - 1)):
+            layers.append(nn.Linear(self.layer_size, self.layer_size))
+            layers.append(nn.ReLU())
+        layers.append(nn.Linear(self.layer_size, output_dim))
+
+        self.nn = nn.Sequential(*layers)
+
+        # define loss and optimizer for training
+        self.loss_fn = nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
 
     def forward(self, x):
         return self.nn(x)
@@ -68,6 +80,15 @@ if __name__ == '__main__':
 
     env = gym.make('Acrobot-v1', render_mode='human')
     observation, info = env.reset()
+
+    # instantiate a parametric neural network
+    model = NN(env, n_layers=3, layer_size=32, lr=1e-3, n_iter=100, max_epochs=100)
+    print('Created NN with {} layers, layer size {}, lr {}'.format(model.n_layers, model.layer_size, model.lr))
+
+    # example forward pass using the current environment state
+    sample_input = get_angular(env).unsqueeze(0)
+    sample_output = model(sample_input)
+    print('Sample network output:', sample_output)
 
     episode_over = False
     it = 0
