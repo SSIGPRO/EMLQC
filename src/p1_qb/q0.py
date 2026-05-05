095dc2f3051e867c757742667803cae8bb183270
Matteo Franceschini <36609943+matteof04@users.noreply.github.com>
2026-05-05 20:01:13 +0200

    Aggiunto secondo qubit

--- src/p1_qb/q0.py
+++ src/p1_qb/q0.py
@@ -1,20 +1,18 @@
 from math import *
 
 from matplotlib import pyplot as plt
-
 from qiskit import QuantumCircuit as QC
 from qiskit import transpile
 from qiskit.primitives import StatevectorSampler
 
-if __name__ == '__main__':
-    qc = QC(1)
-    qc.initialize([1/sqrt(2),1/sqrt(2)], 0)
+if __name__ == "__main__":
+    qc = QC(2)
+    qc.initialize([sqrt(0.1), sqrt(0.2), sqrt(0.3), sqrt(0.4)], [0, 1])
     qc.draw(output="mpl", interactive=True)
     plt.show()
 
     qc_measured = qc.measure_all(inplace=False)
-    sampler = StatevectorSampler()    
+    sampler = StatevectorSampler()
     job = sampler.run([qc_measured], shots=1000)
     result = job.result()
-    print(f" > Counts: {result[0].data["meas"].get_counts()}")
-
+    print(f" > Counts: {result[0].data['meas'].get_counts()}")
