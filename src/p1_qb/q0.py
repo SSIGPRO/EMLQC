105e297266951a380cfec818ae1c76df047ced9e
Fabio Riberti <fabio.riberti.l@gmail.com>
2026-05-06 22:23:38 +0200

    Aggiunta inizializzazione stato a 2 qubit

--- src/p1_qb/q0.py
+++ src/p1_qb/q0.py
@@ -1,20 +1,32 @@
-from math import *
-
+from math import sqrt
 from matplotlib import pyplot as plt
-
 from qiskit import QuantumCircuit as QC
-from qiskit import transpile
 from qiskit.primitives import StatevectorSampler
 
 if __name__ == '__main__':
-    qc = QC(1)
-    qc.initialize([1/sqrt(2),1/sqrt(2)], 0)
+    qc = QC(2)
+    
+    num_samples = 5000 #numero di misurazioni da effettuare
+    
+    state = [sqrt(0.10), sqrt(0.20), sqrt(0.30), sqrt(0.40)]
+    
+    qc.initialize(state, [0, 1])
+    
     qc.draw(output="mpl", interactive=True)
     plt.show()
 
     qc_measured = qc.measure_all(inplace=False)
+    
     sampler = StatevectorSampler()    
-    job = sampler.run([qc_measured], shots=1000)
+    job = sampler.run([qc_measured], shots=num_samples)
     result = job.result()
-    print(f" > Counts: {result[0].data["meas"].get_counts()}")
+    
+    counts = result[0].data['meas'].get_counts()
 
+    print("Valori conteggi:")
+    for key in sorted(counts):
+        print(f"{key}: {counts[key]}")
+
+    print("Valori percentuali:")
+    for key in sorted(counts):
+        print(f"{key}: {counts[key]/(num_samples/100):.2f}%")
