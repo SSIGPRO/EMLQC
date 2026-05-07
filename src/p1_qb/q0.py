a9f52a940a6ca2ab1cf5729aae9322fc49e7fd85
Simux04 <il.tizio.08@gmail.com>
2026-05-07 20:21:12 +0200

    Assignment con entrambe le soluzioni

--- src/p1_qb/q0.py
+++ src/p1_qb/q0.py
@@ -8,13 +8,13 @@ from qiskit.primitives import StatevectorSampler
 
 if __name__ == '__main__':
     qc = QC(2)
-    qc.initialize([sqrt(0.43),sqrt(0.57)], 0)
-    qc.initialize([sqrt(0.29),sqrt(0.71)], 1)
-    
+    #qc.initialize([sqrt(0.1),sqrt(0.2),sqrt(0.3),sqrt(0.4)], [0,1]) Qubit inizializzati insieme
+    qc.initialize([sqrt(0.29),sqrt(0.71)], 0) 
+    qc.initialize([sqrt(0.43),sqrt(0.57)], 1) #qubit inizializzati separatamente
     qc.draw(output="mpl", interactive=True)
     plt.show()
 
-    qc_measured = qc.measure_all(inplace=False)
+    qc_measured = qc1.measure_all(inplace=False)
     sampler = StatevectorSampler()    
     job = sampler.run([qc_measured], shots=1000)
     result = job.result()
