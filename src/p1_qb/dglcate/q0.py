a726ad06e37600796f1700273cbf128d0ba73387
Caterina Degli Angeli <catedegliangeli@gmail.com>
2026-05-07 22:54:10 +0200

    inizializzazione dei due qubit separatamente

--- src/p1_qb/dglcate/q0.py
+++ src/p1_qb/dglcate/q0.py
@@ -14,10 +14,29 @@ if __name__ == '__main__':
     #plt.show()
     
     #inizializzo i due qubits insieme
+    #ora commentato, per l'inizializzazione dei due qubit separatamente
+    #qc = QC(2)
+    #qc.initialize([sqrt(0.1), sqrt(0.2), sqrt(0.3), sqrt(0.4)], [0,1])
+    #qc.draw(output="mpl", interactive=True)
+    #plt.show()
+
+    #inizializzo i due qubit separatamente
     qc = QC(2)
-    qc.initialize([sqrt(0.1), sqrt(0.2), sqrt(0.3), sqrt(0.4)], [0,1])
+    qc.initialize([sqrt(0.1+0.2), sqrt(0.3+0.4)], 0)
+    qc.initialize([sqrt(0.1+0.3), sqrt(0.2+0.4)], 1)
     qc.draw(output="mpl", interactive=True)
     plt.show()
+    #commenti sul metodo:
+    #non si riesce a iniziallizare i due qubit separatamente con un metodo
+    #che sia esattamente equivalente all'inizializzazione contemporanea
+    #Però questo metodo vi si avvicina:
+    #sono partita dal calcolare, per ogni qubit, la probabilità che esso sia o 0 o 1:
+    #trovando che P(|q_0>=0)=0.1+0.2=0.3, P(|q_0>=1)=0.3+0.4=0.7, P(|q_1>=0)=0.1+0.3=0.4, P(|q_1>=1)=0.2+0.4=0.6
+    #Poi ho calcolato la probabilità del sistema formato dai due qubit combinati, calcolando la probabilità di essere in un determinato stato:
+    #P(|00>)=0.3*0.4=0.12, P(|01>)=0.3*0.6=0.18, P(|10>)=0.7*0.4=0.28, P(|11>)=0.7*0.6=0.42
+    #Le probabilità ottetnute (12%, 18%, 28%, 42%) differiscono dalle probabilità target (10%, 20%, 30%, 40%), ma vi si avvicinano
+    #Inoltre, mettendo a sistema 4 equazioni in 4 incognite (una per ogni probabilità di ogni qubit di essere o 0 o 1), cioè i candidati a essere i coefficienti alfa^2,
+    #ed eguagliando ogni equazione alla probabilità target, il sistema risulta impossibile
 
     qc_measured = qc.measure_all(inplace=False)
     sampler = StatevectorSampler()    
