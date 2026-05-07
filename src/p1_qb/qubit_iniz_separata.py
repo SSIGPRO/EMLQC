35067830ba59c0a61abdcb157401d512b8b1a195
Fabio Riberti <fabio.riberti.l@gmail.com>
2026-05-07 21:03:25 +0200

    Inizializzazione separata di due qubit
    Inizializzazione separata di due qubit in stato prodotto.
    Le probabilità degli outcome (12/28/18/42%) sono il prodotto delle ampiezze dei singoli qubit invece dei valori 10/20/30/40%

---
+++ src/p1_qb/qubit_iniz_separata.py
@@ -0,0 +1,34 @@
+from math import sqrt
+from matplotlib import pyplot as plt
+from qiskit import QuantumCircuit as QC
+from qiskit.primitives import StatevectorSampler
+
+if __name__ == '__main__':
+    qc = QC(2)
+    
+    num_samples = 5000
+    
+    state_q0 = [sqrt(0.40), sqrt(0.60)]
+    qc.initialize(state_q0, [0])
+    
+    state_q1 = [sqrt(0.30), sqrt(0.70)] 
+    qc.initialize(state_q1, [1])
+    
+    qc.draw(output="mpl", interactive=True)
+    plt.show()
+
+    qc_measured = qc.measure_all(inplace=False)
+    
+    sampler = StatevectorSampler()    
+    job = sampler.run([qc_measured], shots=num_samples)
+    result = job.result()
+    
+    counts = result[0].data['meas'].get_counts()
+
+    print("Valori conteggi:")
+    for key in sorted(counts):
+        print(f"{key}: {counts[key]}")
+
+    print("Valori percentuali:") #
+    for key in sorted(counts):
+        print(f"{key}: {counts[key]/(num_samples/100):.2f}%")
