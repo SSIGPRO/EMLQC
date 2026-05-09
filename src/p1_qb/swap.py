cd1eefb518dba863d9361a53306a3cbc1d3d8b3e
Fabio Riberti <fabio.riberti.l@gmail.com>
2026-05-09 15:57:51 +0200

    Update swap.py

--- src/p1_qb/swap.py
+++ src/p1_qb/swap.py
@@ -1,54 +1,37 @@
 from math import *
-import torch
+
 import numpy as np
+import torch
 from matplotlib import pyplot as plt
 from qiskit import QuantumCircuit as QC
 from qiskit import transpile
 from qiskit.primitives import StatevectorSampler as Sampler
 from qiskit.visualization import plot_histogram
 
-if __name__ == '__main__':
-    sampler = Sampler()    
-    num_samples = 10000
-    
-    c0 = np.sqrt(np.array([0.8, 0.2]))
-    c1 = np.sqrt(np.array([0.3, 0.7]))
+if __name__ == "__main__":
+    sampler = Sampler()
+    c0 = np.sqrt(np.array([0.3, 0.7]))
+    c1 = np.sqrt(np.array([0.8, 0.2]))
 
     qc = QC(2, 2)
     qc.initialize(c0, 0)
     qc.initialize(c1, 1)
-    
     qc_measured = qc.measure_all(inplace=False, add_bits=False)
-    
-    qc.swap(0, 1)
-    
+    qc.cx(0, 1)
+    qc.cx(1, 0)
+    qc.cx(0, 1)
     qc.measure(qubit=1, cbit=1)
     qc.measure(qubit=0, cbit=0)
-    
     qc.draw(output="mpl", interactive=True)
-    
-    job_before = sampler.run([qc_measured], shots=num_samples)
-    
+    job_before = sampler.run([qc_measured], shots=1000)
     result_before = job_before.result()
     counts_before = result_before[0].data["c"].get_counts()
-    
-    probs_before = {
-        stato: conteggio / num_samples  
-        for stato, conteggio in counts_before.items()
-    }
-    plot_histogram(probs_before, title="circuito prima dello swap")
-    
-    job = sampler.run([qc], shots=num_samples)
+    plot_histogram(counts_before)
+    job = sampler.run([qc], shots=1000)
     result = job.result()
     print(result)
     print(result[0].data)
-    counts_after = result[0].data["c"].get_counts()
-    
-    probs_after = {
-        stato: conteggio / num_samples  
-        for stato, conteggio in counts_after.items()
-    }
-    plot_histogram(probs_after, title="circuito dopo lo swap")
-    
-    print(f" > Counts: {counts_after}")
+    counts = result[0].data["c"].get_counts()
+    plot_histogram(counts)
+    print(f" > Counts: {counts}")
     plt.show()
