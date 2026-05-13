7ca59c273f5bc9924efaebcb2000e643561ada09
Matteo Franceschini <36609943+matteof04@users.noreply.github.com>
2026-05-13 10:19:12 +0200

    Added or gate

---
+++ src/p1_qb/orgate.py
@@ -0,0 +1,39 @@
+from math import *
+
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+from qiskit import QuantumCircuit as QC
+from qiskit import transpile
+from qiskit.primitives import StatevectorSampler as Sampler
+from qiskit.visualization import plot_histogram
+
+if __name__ == "__main__":
+    sampler = Sampler()
+    c0 = np.sqrt(np.array([0.25, 0.75]))
+    c1 = np.sqrt(np.array([0.1, 0.9]))
+    c2 = np.sqrt(np.array([1.0, 0.0]))
+
+    qc = QC(3, 3)
+    qc.initialize(c0, 0)
+    qc.initialize(c1, 1)
+    qc.initialize(c2, 2)
+    qc.x(0)
+    qc.x(1)
+    qc.ccx(0, 1, 2)
+    qc.x(0)
+    qc.x(1)
+    qc.x(2)
+    qc.measure(qubit=0, cbit=0)
+    qc.measure(qubit=1, cbit=1)
+    qc.measure(qubit=2, cbit=2)
+    qc.draw(output="mpl", interactive=True)
+
+    job = sampler.run([qc], shots=1000)
+    result = job.result()
+    print(result)
+    print(result[0].data)
+    counts = result[0].data["c"].get_counts()
+    plot_histogram(counts)
+    print(f" > Counts: {counts}")
+    plt.show()
