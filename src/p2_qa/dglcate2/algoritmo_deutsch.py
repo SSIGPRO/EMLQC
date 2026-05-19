66958fae878cf7ef2e8ad6bdf641c8932d67029b
Caterina Degli Angeli <catedegliangeli@gmail.com>
2026-05-19 17:22:45 +0200

    Deutsch's Algorithm

---
+++ src/p2_qa/dglcate2/algoritmo_deutsch.py
@@ -0,0 +1,52 @@
+from math import *
+
+import torch
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+from qiskit import QuantumCircuit as QC
+from qiskit import transpile
+from qiskit.primitives import StatevectorSampler as Sampler
+from qiskit.visualization import plot_histogram
+
+def f00(qc):
+    return
+
+def f01(qc):
+    qc.cx(0,1)
+    return
+
+def f10(qc):
+    qc.cx(0,1)
+    qc.x(1)
+    return
+
+def f11(qc):
+    qc.x(1)
+    return
+
+if __name__ == '__main__':
+    sampler = Sampler()    
+    
+    qcs = []
+    for function in [f00, f01, f10, f11]:
+        qc = QC(2, 1)
+        qc.initialize([1.0, 0.0], 0)
+        qc.initialize([0.0, 1.0], 1)
+        qc.h(0)
+        qc.h(1)
+        function(qc)
+        qc.h(0)
+        qc.h(1)
+        qc.measure(qubit=0,cbit=0)
+        qc.draw(output="mpl", interactive=True)
+        qcs.append(qc)
+
+    job = sampler.run(qcs, shots=1000)
+    results = job.result()
+    for res in results:
+        counts = res.data['c'].get_counts()
+        plot_histogram(counts)
+        print(f" > Counts: {counts}")
+    plt.show()
