92e564c3b356bda4ac66c2cff8cc17f9afcc9d2d
frezyes <fabio.riberti.l@gmail.com>
2026-05-22 18:51:26 +0200

    modifiche al file q7 per l'assigment

--- src/p2_qa/q7.py
+++ src/p2_qa/q7.py
@@ -1,47 +1,54 @@
-from math import *
-
-import torch
 import numpy as np
-
 from matplotlib import pyplot as plt
-
 from qiskit import QuantumCircuit as QC
+from qiskit.circuit.gate import Gate
+from qiskit.circuit.library.basis_change.qft import QFTGate
+from qiskit.circuit.quantumcircuit import QuantumCircuit
 from qiskit.primitives import StatevectorSampler as Sampler
-from qiskit.visualization import plot_histogram, plot_bloch_multivector, plot_state_qsphere
 from qiskit.quantum_info import Statevector
-from qiskit.circuit.library import QFT
+from qiskit.visualization import (plot_bloch_multivector, plot_histogram, plot_state_qsphere)
+
+
+def f(theta: float) -> Gate:
+    qc = QuantumCircuit(1, name="F")
+    qc.h(0)
+    qc.ry(theta, 0)
+    qc.rz(theta, 0)
+    
+    return qc.to_gate().control(1)
+
 
 def get_qc(theta):
     qc = QC(3, 2)
+    
     qc.x(2)
     qc.barrier()
-                             
+
     qc.h(0)
     qc.h(1)
     qc.barrier()
-    
-    qc.cp(2*pi*theta, 0, 2)
 
+    gate = f(theta)
+    
+    qc.append(gate, [0, 2])
     qc.barrier()
 
-    qc.cp(2*pi*theta, 1, 2)
-    qc.cp(2*pi*theta, 1, 2)
-    
+    qc.append(gate, [1, 2])
+    qc.append(gate, [1, 2])
     qc.barrier()
-    qc.compose(
-        QFT(2, inverse=True).decompose(),
-        inplace=True
-    )
+
+    qc.compose(QFTGate(2).inverse(), qubits=[0, 1], inplace=True)
     return qc
 
-if __name__ == '__main__':
-    sampler = Sampler()    
+if __name__ == "__main__":
+    sampler = Sampler()
     shots = 1000
-    theta = 1/4
-   
-    qc1 = get_qc(theta)
+    
+    theta = np.pi / 2
 
+    qc1 = get_qc(theta)
     qc2 = get_qc(theta)
+    
     qc2.measure(qubit=0, cbit=0)
     qc2.measure(qubit=1, cbit=1)
 
@@ -49,10 +56,9 @@ if __name__ == '__main__':
 
     job = sampler.run([qc2], shots=shots)
     results = job.result()
-    counts = results[0].data['c'].get_counts()
-    plot_histogram({k:c/shots for k, c in counts.items()})
+    counts = results[0].data["c"].get_counts()
+    plot_histogram({k: c / shots for k, c in counts.items()})
 
     plot_bloch_multivector(Statevector(qc1))
     plot_state_qsphere(qc1)
     plt.show()
-
