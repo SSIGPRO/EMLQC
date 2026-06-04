b11da76b781f6571cad8f34cea97f6c79fb58f35
Matteo Franceschini <36609943+matteof04@users.noreply.github.com>
2026-06-04 12:21:33 +0200

    Added assignment due to June 5

--- src/p3_ga/q0.py
+++ src/p3_ga/q0.py
@@ -1,39 +1,45 @@
-from math import *
+from math import floor, pi, sqrt
 
-import torch
 import numpy as np
-
 from matplotlib import pyplot as plt
-
 from qiskit import QuantumCircuit as QC
-from qiskit import transpile
+from qiskit.circuit.library.grover_operator import grover_operator
 from qiskit.primitives import StatevectorSampler as Sampler
 from qiskit.visualization import plot_histogram
-from qiskit.circuit.library import GroverOperator
+
 
 def n_iters(nqb):
     np = 2**nqb
-    ni = (pi/4)*sqrt(np)
+    ni = (pi / 4) * sqrt(np)
     return floor(ni)
 
-def get_oracle(nqb):
+
+def get_oracle(nqb: int, key: int):
     qc = QC(nqb)
-    qc.x(0)
-    qc.cz(0,1)
-    qc.x(0)
+    if key == 0b00 or key == 0b01:
+        qc.x(0)
+    if key == 0b00 or key == 0b10:
+        qc.x(1)
+    qc.cz(0, 1)
+    if key == 0b00 or key == 0b01:
+        qc.x(0)
+    if key == 0b00 or key == 0b10:
+        qc.x(1)
     return qc
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     nqb = 2
+    key = 0b11
     ni = n_iters(nqb)
-    print('niter: ', ni)
+    print("niter: ", ni)
     shots = 1000
-    sampler = Sampler()    
-    
+    sampler = Sampler()
+
     # Grover's stuff
-    oracle = get_oracle(nqb) 
-    grover_op = GroverOperator(oracle)
-    
+    oracle = get_oracle(nqb, key)
+    grover_op = grover_operator(oracle)
+
     # circuit
     qc = QC(nqb)
     qc.h(np.arange(nqb))
@@ -43,12 +49,11 @@ if __name__ == '__main__':
     qc.barrier()
 
     qc.draw(output="mpl", interactive=True)
-    
+
     qcm = qc.measure_all(inplace=False)
-    job = sampler.run([qcm], shots=shots)
+    job = sampler.run([qcm], shots=shots)  # ty:ignore[invalid-argument-type]
     result = job.result()
-    counts = {k:v/shots for k, v in result[0].data['meas'].get_counts().items()}
+    counts = {k: v / shots for k, v in result[0].data["meas"].get_counts().items()}
     plot_histogram(counts)
     print(f" > Counts: {counts}")
     plt.show()
-
