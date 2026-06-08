241175a127b404e678c7b3f0ab25c77114807e64
Bedavide04 <benincasa.davide04@gmail.com>
2026-06-08 19:41:37 +0200

    Ultimo esercizio

--- src/p3_ga/q1.py
+++ src/p3_ga/q1.py
@@ -1,89 +1,80 @@
-from math import *
-
-import torch
+from math import pi, floor, asin, sqrt
 import numpy as np
-
 from matplotlib import pyplot as plt
-
-from qiskit import QuantumCircuit as QC, QuantumRegister
-from qiskit import transpile
+from qiskit import QuantumCircuit as QC, QuantumRegister, ClassicalRegister
 from qiskit.primitives import StatevectorSampler as Sampler
-from qiskit.visualization import plot_histogram, plot_bloch_multivector
+from qiskit.visualization import plot_histogram
+from qiskit.circuit.library import GroverOperator, QuadraticForm
 
-from qiskit.circuit.library import GroverOperator, MCMTGate, ZGate
-from qiskit.circuit.library import DraperQFTAdder as Add
+def n_iters(nqb, n_sol=1):
+    return floor(pi / (4 * asin(sqrt(n_sol / 2**nqb))))
 
-def n_iters(nqb):
-    ni = pi / (4 * asin(sqrt(1 / 2**nqb)))
-    #ni = (pi/4)*sqrt(2**nqb)
-    return floor(ni)
+def get_oracle(n_x: int, n_res: int):
+    x   = QuantumRegister(n_x,  'x')
+    res = QuantumRegister(n_res, 'res')
+    qc  = QC(x, res)
 
-def get_oracle():
-    a = QuantumRegister(2, 'a')
-    b = QuantumRegister(2, 'b')
-    c = QuantumRegister(2, 'c')
+    A = [[0, 1],
+         [0, 1]]
+    b = [-1, -2]
+    c = 0
 
-    qc = QC(*[a, b, c])
-
-    # make a = b'10 = d'2
-    qc.x(a[1])
-
-    # First H from marking 
-    qc.h(b)
-
-    # make c = b'11b, d'3
-    qc.x(c)
-    qc.barrier() 
+    qf = QuadraticForm(
+        num_result_qubits = n_res,
+        quadratic = A,
+        linear    = b,
+        offset    = c,
+        little_endian = True
+    )
     
-    adder = Add(2, kind='fixed', name='Add')
-    suber = Add(2, kind='fixed', name='Sub').inverse()
-
-    qc.append(adder, a[:]+b[:])
-    qc.append(suber, c[:]+b[:])
-    qc.barrier() 
-
-    # if the a+b == c, the output (on qubits b will be 00)
-    # Invert phase on 00 
-    qc.x(b)
-    qc.compose(MCMTGate(ZGate(), 1, 1), b[:], inplace=True) 
-    qc.x(b)
+    qc.append(qf, x[:] + res[:])
     qc.barrier()
 
-    # uncompute f
-    qc.append(suber.inverse(), c[:]+b[:])
-    qc.append(adder.inverse(), a[:]+b[:])
-    # Last H from the marking
-    qc.h(b)
+    qc.x(res)
+    qc.h(res[-1])
+    qc.mcx(res[:-1], res[-1])   
+    qc.h(res[-1])
+    qc.x(res)
     qc.barrier()
+
+    qc.append(qf.inverse(), x[:] + res[:])
 
     return qc
 
 if __name__ == '__main__':
-    nqb = 2
-    ni = n_iters(nqb)
-    print('niter: ', ni)
-    shots = 1000
-    sampler = Sampler()    
-    
-    # Grover's stuff
-    oracle = get_oracle() 
-    grover_op = GroverOperator(oracle, reflection_qubits = [2, 3])
-    oracle.draw(output="mpl", interactive=True)
+    n_x   = 2
+    n_res = 3
+    n_sol = 2
 
-    # circuit
-    qc = QC(6, 2)
+    ni    = n_iters(n_x, n_sol)
+    shots = 1000
     
+    print(f"(x-1)^2 = 1  →  soluzioni: x=0, x=2")
+    print(f"n_iter: {ni}")
+
+    sampler = Sampler()
+
+    oracle     = get_oracle(n_x, n_res)
+    grover_op  = GroverOperator(oracle, reflection_qubits=list(range(n_x)))
+
+    total_qb = n_x + n_res
+    
+    c_reg = ClassicalRegister(n_x, 'c')
+    qc = QC(QuantumRegister(total_qb, 'q'), c_reg)
+    
+    qc.h(range(n_x))
     qc.barrier()
     qc.compose(grover_op.power(ni), inplace=True)
     qc.barrier()
-    qc.measure([2, 3], [0, 1])
+    qc.measure(range(n_x), range(n_x))
 
-    qc.draw(output="mpl", interactive=True)
-    
-    job = sampler.run([qc], shots=shots)
+    job    = sampler.run([qc], shots=shots)
     result = job.result()
-    counts = {k:v/shots for k, v in result[0].data['c'].get_counts().items()}
-    plot_histogram(counts)
-    print(f" > Counts: {counts}")
-    plt.show()
+    
+    counts = {k: v/shots for k, v in result[0].data.c.get_counts().items()}
 
+    decimal_counts = {f"|{b}⟩ x={int(b,2)}": p for b, p in counts.items()}
+    print(f"Counts: {decimal_counts}")
+
+    plot_histogram(decimal_counts, title="(x-1)²=1 — Grover + QuadraticForm")
+    plt.show()
